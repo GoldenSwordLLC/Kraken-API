5186a4c0eaad2345e437e486f0a246769df8c1e6
Scott King <Lvl4Sword@users.noreply.github.com>
2023-02-18 21:40:21 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Base64 string of the key
+api_key_b = ""
+# Base64 string of the secret
+api_secret_b = ""
+api_url = "https://api.kraken.com"
+account_type = 'intermediate'
+
+# GUI information
+verify_closing = True
